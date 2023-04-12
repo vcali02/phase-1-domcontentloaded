943c74fa86863f5d65f5531a5abfff15680f3192
valeria caliguire <caliguireval@gmail.com>
2023-04-12 07:10:57 -0600

    DOMContentLoaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+//set up DOMContentLoaded event listener to detect
+//when HTML page has loaded and doc is ready to be manipulated
+
+document.addEventListener("DOMContentLoaded", function(){
+    console.log(document.getElementById("text").textContent = "This is really cool!");
+})
+//callback function grabs the id ="text" and changes the value to
+//"This is really cool!"
